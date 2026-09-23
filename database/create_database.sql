68057b1822e5d27ba6ecd7ff96fa33293717fba3
HEA2025 <hedi94220@gmail.com>
2026-09-23 21:04:48 +0200

    docs: regenere scripts sql finaux

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,3 +1,8 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vite_et_gourmand
+-- ------------------------------------------------------
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -9,6 +14,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `allergene`
+--
+
 DROP TABLE IF EXISTS `allergene`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -16,8 +26,13 @@ CREATE TABLE `allergene` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `avis`
+--
+
 DROP TABLE IF EXISTS `avis`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -31,8 +46,13 @@ CREATE TABLE `avis` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_8F91ABF082EA2E54` (`commande_id`),
   CONSTRAINT `FK_8F91ABF082EA2E54` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commande`
+--
+
 DROP TABLE IF EXISTS `commande`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -48,11 +68,24 @@ CREATE TABLE `commande` (
   `prix_total` decimal(10,2) NOT NULL,
   `date_creation` datetime NOT NULL,
   `menu_id` int NOT NULL,
+  `utilisateur_id` int NOT NULL,
+  `distance_livraison` double DEFAULT NULL,
+  `materiel_prete` tinyint NOT NULL DEFAULT '0',
+  `moyen_contact_employe` varchar(20) DEFAULT NULL,
+  `motif_intervention_employe` longtext,
+  `date_contact_employe` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_6EEAA67DCCD7E912` (`menu_id`),
-  CONSTRAINT `FK_6EEAA67DCCD7E912` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `IDX_6EEAA67DFB88E14F` (`utilisateur_id`),
+  CONSTRAINT `FK_6EEAA67DCCD7E912` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`),
+  CONSTRAINT `FK_6EEAA67DFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `doctrine_migration_versions`
+--
+
 DROP TABLE IF EXISTS `doctrine_migration_versions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -63,6 +96,11 @@ CREATE TABLE `doctrine_migration_versions` (
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `historique_statut_commande`
+--
+
 DROP TABLE IF EXISTS `historique_statut_commande`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -74,8 +112,13 @@ CREATE TABLE `historique_statut_commande` (
   PRIMARY KEY (`id`),
   KEY `IDX_CAB4641E82EA2E54` (`commande_id`),
   CONSTRAINT `FK_CAB4641E82EA2E54` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `horaire`
+--
+
 DROP TABLE IF EXISTS `horaire`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -85,8 +128,13 @@ CREATE TABLE `horaire` (
   `heure_ouverture` time NOT NULL,
   `heure_fermeture` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `image_menu`
+--
+
 DROP TABLE IF EXISTS `image_menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -97,8 +145,13 @@ CREATE TABLE `image_menu` (
   PRIMARY KEY (`id`),
   KEY `IDX_8F3FD00DCCD7E912` (`menu_id`),
   CONSTRAINT `FK_8F3FD00DCCD7E912` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `menu`
+--
+
 DROP TABLE IF EXISTS `menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -117,8 +170,13 @@ CREATE TABLE `menu` (
   KEY `IDX_7D053A9335E7D534` (`regime_id`),
   CONSTRAINT `FK_7D053A9335E7D534` FOREIGN KEY (`regime_id`) REFERENCES `regime` (`id`),
   CONSTRAINT `FK_7D053A9359027487` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `menu_plat`
+--
+
 DROP TABLE IF EXISTS `menu_plat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -132,6 +190,11 @@ CREATE TABLE `menu_plat` (
   CONSTRAINT `FK_E8775249D73DB560` FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plat`
+--
+
 DROP TABLE IF EXISTS `plat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -141,8 +204,13 @@ CREATE TABLE `plat` (
   `description` longtext NOT NULL,
   `type` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plat_allergene`
+--
+
 DROP TABLE IF EXISTS `plat_allergene`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -156,6 +224,11 @@ CREATE TABLE `plat_allergene` (
   CONSTRAINT `FK_6FA44BBFD73DB560` FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `regime`
+--
+
 DROP TABLE IF EXISTS `regime`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -163,8 +236,33 @@ CREATE TABLE `regime` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reset_password_request`
+--
+
+DROP TABLE IF EXISTS `reset_password_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reset_password_request` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `selector` varchar(20) NOT NULL,
+  `hashed_token` varchar(100) NOT NULL,
+  `requested_at` datetime NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_7CE748AA76ED395` (`user_id`),
+  CONSTRAINT `FK_7CE748AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `theme`
+--
+
 DROP TABLE IF EXISTS `theme`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -172,7 +270,29 @@ CREATE TABLE `theme` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(20) DEFAULT NULL,
+  `prenom` varchar(20) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `actif` tinyint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -184,3 +304,4 @@ CREATE TABLE `theme` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2026-09-23 20:58:58
--- database/insert_data.sql
+++ database/insert_data.sql
@@ -1,3 +1,8 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vite_et_gourmand
+-- ------------------------------------------------------
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -10,52 +15,155 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Dumping data for table `allergene`
+--
+
+LOCK TABLES `allergene` WRITE;
 /*!40000 ALTER TABLE `allergene` DISABLE KEYS */;
-INSERT INTO `allergene` VALUES (5,'Gluten'),(6,'Lait'),(7,'Œufs'),(8,'Fruits à coque');
+INSERT INTO `allergene` VALUES (17,'Gluten'),(18,'Lait'),(19,'Œufs'),(20,'Fruits à coque');
 /*!40000 ALTER TABLE `allergene` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `avis`
+--
+
+LOCK TABLES `avis` WRITE;
 /*!40000 ALTER TABLE `avis` DISABLE KEYS */;
+INSERT INTO `avis` VALUES (1,5,'delicieux','validé','2026-09-19 15:41:37',4),(2,5,'Excellent','validé','2026-09-21 13:32:37',13);
 /*!40000 ALTER TABLE `avis` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `commande`
+--
+
+LOCK TABLES `commande` WRITE;
 /*!40000 ALTER TABLE `commande` DISABLE KEYS */;
+INSERT INTO `commande` VALUES (1,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-25','12:30:00',2,45.00,0.00,0.00,45.00,'2026-09-19 12:54:19',8,7,0,0,NULL,NULL,NULL),(2,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-25','12:30:00',4,45.00,0.00,0.00,45.00,'2026-09-19 12:54:42',8,7,0,1,'gsm','Modification demandée par le client','2026-09-19 15:27:04'),(3,'1 avenue de Paris, 33600 Pessac','2026-09-25','12:30:00',7,45.00,4.50,10.90,51.40,'2026-09-19 12:55:58',8,7,10,0,NULL,NULL,NULL),(4,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-27','12:30:00',2,45.00,0.00,0.00,45.00,'2026-09-19 13:18:37',8,7,0,0,NULL,NULL,NULL),(5,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-26','12:30:00',2,45.00,0.00,0.00,45.00,'2026-09-19 14:47:12',8,6,0,0,NULL,NULL,NULL),(6,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-26','12:30:00',7,45.00,4.50,0.00,40.50,'2026-09-19 14:47:35',8,6,0,0,NULL,NULL,NULL),(7,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-26','12:30:00',7,45.00,4.50,10.90,51.40,'2026-09-19 14:47:50',8,6,10,0,NULL,NULL,NULL),(8,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-28','14:30:00',8,45.00,4.50,0.00,40.50,'2026-09-19 16:04:54',8,7,0,0,NULL,NULL,NULL),(9,'1 avenue de Paris, 33600 Pessac','2026-09-22','14:00:00',4,35.00,0.00,10.90,45.90,'2026-09-20 18:49:30',9,6,10,0,NULL,NULL,NULL),(10,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-23','12:00:00',8,35.00,3.50,0.00,31.50,'2026-09-20 18:52:28',9,6,0,0,NULL,NULL,NULL),(11,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-22','11:30:00',4,70.00,0.00,0.00,70.00,'2026-09-20 19:18:04',9,6,0,0,NULL,NULL,NULL),(12,'1 avenue de Paris, 33600 Pessac','2026-09-25','20:00:00',8,140.00,14.00,10.90,136.90,'2026-09-20 19:20:10',9,6,10,0,NULL,NULL,NULL),(13,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-25','19:20:00',2,45.00,0.00,0.00,45.00,'2026-09-21 13:18:11',8,10,0,0,NULL,NULL,NULL),(14,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-22','14:00:00',3,52.50,0.00,0.00,52.50,'2026-09-22 11:49:57',9,10,0,0,NULL,NULL,NULL),(15,'10 rue Sainte-Catherine, 33000 Bordeaux','2026-09-22','14:00:00',3,52.50,0.00,0.00,52.50,'2026-09-22 11:54:33',9,10,0,0,'email','Erreur d\'adresse','2026-09-22 11:57:10');
 /*!40000 ALTER TABLE `commande` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
+
+LOCK TABLES `doctrine_migration_versions` WRITE;
 /*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
-INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20260915140711','2026-09-15 14:19:51',1693);
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20260915140711','2026-09-15 14:19:51',1693),('DoctrineMigrations\\Version20260916095242','2026-09-16 09:53:45',81),('DoctrineMigrations\\Version20260916203049','2026-09-16 20:36:10',20),('DoctrineMigrations\\Version20260917145143','2026-09-17 14:52:29',178),('DoctrineMigrations\\Version20260917160835','2026-09-17 16:09:24',181),('DoctrineMigrations\\Version20260917200517','2026-09-17 20:12:41',84),('DoctrineMigrations\\Version20260919123917','2026-09-19 12:39:35',59),('DoctrineMigrations\\Version20260919145735','2026-09-19 14:58:02',56);
 /*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `historique_statut_commande`
+--
+
+LOCK TABLES `historique_statut_commande` WRITE;
 /*!40000 ALTER TABLE `historique_statut_commande` DISABLE KEYS */;
+INSERT INTO `historique_statut_commande` VALUES (1,'en attente','2026-09-19 13:18:37',4),(2,'acceptée','2026-09-19 13:38:42',4),(3,'en préparation','2026-09-19 13:40:05',4),(4,'en cours de livraison','2026-09-19 13:41:44',4),(5,'livrée','2026-09-19 13:42:20',4),(6,'terminée','2026-09-19 13:42:27',4),(7,'en attente','2026-09-19 14:47:12',5),(8,'en attente','2026-09-19 14:47:35',6),(9,'en attente','2026-09-19 14:47:50',7),(10,'acceptée','2026-09-19 15:27:49',2),(11,'en préparation','2026-09-19 15:27:59',2),(12,'en cours de livraison','2026-09-19 15:28:08',2),(13,'livrée','2026-09-19 15:28:33',2),(14,'acceptée','2026-09-19 15:28:53',2),(15,'en attente du retour de matériel','2026-09-19 15:29:07',2),(16,'terminée','2026-09-19 15:29:58',2),(17,'en attente','2026-09-19 16:04:55',8),(18,'annulée','2026-09-19 16:05:48',1),(19,'en attente','2026-09-20 18:49:31',9),(20,'en attente','2026-09-20 18:52:28',10),(21,'en attente','2026-09-20 19:18:04',11),(22,'en attente','2026-09-20 19:20:11',12),(23,'en attente','2026-09-21 13:18:11',13),(24,'acceptée','2026-09-21 13:21:21',13),(25,'acceptée','2026-09-21 13:22:00',12),(26,'acceptée','2026-09-21 13:22:19',11),(27,'acceptée','2026-09-21 13:22:47',10),(28,'acceptée','2026-09-21 13:23:00',9),(29,'acceptée','2026-09-21 13:23:13',8),(30,'acceptée','2026-09-21 13:23:22',7),(31,'en préparation','2026-09-21 13:23:50',4),(32,'acceptée','2026-09-21 13:24:06',3),(33,'en préparation','2026-09-21 13:24:23',5),(34,'en préparation','2026-09-21 13:24:41',3),(35,'en cours de livraison','2026-09-21 13:24:56',3),(36,'en cours de livraison','2026-09-21 13:25:08',5),(37,'en cours de livraison','2026-09-21 13:25:19',4),(38,'en cours de livraison','2026-09-21 13:25:33',10),(39,'en cours de livraison','2026-09-21 13:25:44',11),(40,'en cours de livraison','2026-09-21 13:26:00',9),(41,'livrée','2026-09-21 13:26:11',11),(42,'livrée','2026-09-21 13:26:23',10),(43,'livrée','2026-09-21 13:26:34',9),(44,'en cours de livraison','2026-09-21 13:27:02',13),(45,'livrée','2026-09-21 13:27:15',13),(46,'terminée','2026-09-21 13:27:35',13),(47,'en attente','2026-09-22 11:49:57',14),(48,'annulée','2026-09-22 11:52:07',14),(49,'en attente','2026-09-22 11:54:33',15),(50,'annulée','2026-09-22 11:57:10',15);
 /*!40000 ALTER TABLE `historique_statut_commande` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `horaire`
+--
+
+LOCK TABLES `horaire` WRITE;
 /*!40000 ALTER TABLE `horaire` DISABLE KEYS */;
+INSERT INTO `horaire` VALUES (1,'Lundi','09:00:00','23:00:00'),(2,'Mardi','09:30:00','22:00:00'),(3,'Mercredi','10:00:00','22:30:00'),(4,'Jeudi','10:30:00','23:00:00'),(5,'Vendredi','09:15:00','22:40:00'),(6,'Samedi','10:00:00','00:00:00'),(7,'Dimanche','09:00:00','16:00:00');
 /*!40000 ALTER TABLE `horaire` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `image_menu`
+--
+
+LOCK TABLES `image_menu` WRITE;
 /*!40000 ALTER TABLE `image_menu` DISABLE KEYS */;
+INSERT INTO `image_menu` VALUES (5,'menu-gourmand.png',8),(6,'plat-gourmand.png',8),(7,'dessert-chocolat.png',8),(8,'menu-vegetarien.png',9);
 /*!40000 ALTER TABLE `image_menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'Menu Gourmand','Un menu gourmand et traditionnel.',2,45.00,'Commande au minimum 48 heures à l’avance.',10,5,4),(2,'Menu Végétarien','Un menu sans viande à base de légumes de saison.',2,35.00,'Commande au minimum 48 heures à l’avance.',8,5,5);
+INSERT INTO `menu` VALUES (8,'Menu Gourmand','Un menu gourmand et traditionnel.',2,45.00,'Commande au minimum 48 heures à l’avance.',10,17,13),(9,'Menu Végétarien','Un menu sans viande à base de légumes de saison.',2,35.00,'Commande au minimum 48 heures à l’avance.',8,17,14),(10,'Menu Vegan','un repas composé exclusivement d\'aliments d\'origine végétale.',2,35.00,'Conditions du menu\nCommande au minimum 48 heures à l’avance.',4,17,13),(12,'Menu Méditerranéen','Un menu frais et gourmand aux saveurs méditerranéennes.',4,80.00,'Conditions du menu\nCommande au minimum 12 heures à l’avance.',4,17,13);
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `menu_plat`
+--
+
+LOCK TABLES `menu_plat` WRITE;
 /*!40000 ALTER TABLE `menu_plat` DISABLE KEYS */;
-INSERT INTO `menu_plat` VALUES (1,1),(1,2),(1,4),(2,1),(2,3),(2,5);
+INSERT INTO `menu_plat` VALUES (8,17),(8,18),(8,20),(9,17),(9,19),(9,21),(10,17),(10,23),(10,29),(12,18),(12,25),(12,29);
 /*!40000 ALTER TABLE `menu_plat` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `plat`
+--
+
+LOCK TABLES `plat` WRITE;
 /*!40000 ALTER TABLE `plat` DISABLE KEYS */;
-INSERT INTO `plat` VALUES (1,'Salade gourmande','Salade fraîche aux légumes de saison.','entree'),(2,'Saumon rôti','Saumon rôti accompagné de légumes.','plat'),(3,'Gratin de légumes','Gratin de légumes de saison.','plat'),(4,'Fondant au chocolat','Fondant au chocolat maison.','dessert'),(5,'Salade de fruits','Fruits frais de saison.','dessert');
+INSERT INTO `plat` VALUES (17,'Salade gourmande','Salade fraîche aux légumes de saison.','entrée'),(18,'Saumon rôti','Saumon rôti accompagné de légumes.','plat'),(19,'Gratin de légumes','Gratin de légumes de saison.','plat'),(20,'Fondant au chocolat','Fondant au chocolat maison.','dessert'),(21,'Salade de fruits','Fruits frais de saison.','dessert'),(23,'Curry de poid chiches','Curry onctueux de pois chiches, lentilles corail et jeunes épinards au lait de coco infusé aux épices douces, servi avec son riz basmati.','plat'),(25,'Salade méditerranéenne','Salade fraîche composée de tomates, concombre, olives et herbes aromatiques.','entrée'),(29,'Soupe de fruits rouges','Fruits rouges fraîchement infusée à la menthe, surmontée d\'un sorbet à la framboise maison.','dessert');
 /*!40000 ALTER TABLE `plat` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `plat_allergene`
+--
+
+LOCK TABLES `plat_allergene` WRITE;
 /*!40000 ALTER TABLE `plat_allergene` DISABLE KEYS */;
-INSERT INTO `plat_allergene` VALUES (3,6),(4,5),(4,6),(4,7);
+INSERT INTO `plat_allergene` VALUES (19,18),(20,17),(20,18),(20,19);
 /*!40000 ALTER TABLE `plat_allergene` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `regime`
+--
+
+LOCK TABLES `regime` WRITE;
 /*!40000 ALTER TABLE `regime` DISABLE KEYS */;
-INSERT INTO `regime` VALUES (4,'Classique'),(5,'Végétarien'),(6,'Vegan');
+INSERT INTO `regime` VALUES (13,'Classique'),(14,'Végétarien'),(15,'Vegan');
 /*!40000 ALTER TABLE `regime` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `reset_password_request`
+--
+
+LOCK TABLES `reset_password_request` WRITE;
+/*!40000 ALTER TABLE `reset_password_request` DISABLE KEYS */;
+INSERT INTO `reset_password_request` VALUES (1,'Uvkanrxxb381LyaKTNzP','8s+OP6n7Rgq4k8bc0l90E4PC8aC2wGdj1DbqY5N+jqk=','2026-09-21 10:06:50','2026-09-21 11:06:50',7);
+/*!40000 ALTER TABLE `reset_password_request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `theme`
+--
+
+LOCK TABLES `theme` WRITE;
 /*!40000 ALTER TABLE `theme` DISABLE KEYS */;
-INSERT INTO `theme` VALUES (5,'Classique'),(6,'Noël'),(7,'Pâques'),(8,'Événement');
+INSERT INTO `theme` VALUES (17,'Classique'),(18,'Noël'),(19,'Pâques'),(20,'Événement');
 /*!40000 ALTER TABLE `theme` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+INSERT INTO `utilisateur` VALUES (6,'jose@vite-et-gourmand.fr','[\"ROLE_ADMINISTRATEUR\"]','$2y$13$q/2/sYf7XTf/ISkxTOshF.NNHq1RUj/qhB5R9ByrZobqhELlHl2eS','José','José','0600000000','Adresse à compléter',1),(7,'jean@vite-et-gourmand.fr','[\"ROLE_USER\"]','$2y$13$7B/8L5.hkn1eTn5W8hhFw.g3ZzEvE.6o/ABegDeqA8bS0JMofcWuq','Dupont','Jean','0612345678','10 rue Sainte-Catherine, 33000 Bordeaux',1),(8,'employe.test@vite-et-gourmand.fr','[\"ROLE_EMPLOYE\"]','$2y$13$tO3RFa/5VKzMo1Mvl35roufM7Cnvg1arPW8AZHwnaEdNAaIenG06i',NULL,NULL,NULL,NULL,0),(9,'employe@vite-et-gourmand.fr','[\"ROLE_EMPLOYE\"]','$2y$13$cd9qvwtnUXbosgPyd4U68O4jeTO.Ayn/YZfNCqD34akx9Y6WfUr9.',NULL,NULL,NULL,NULL,1),(10,'Steve@gmail.com','[\"ROLE_USER\"]','$2y$13$gmLDVQ.m7jcBHQgJzTejCetj5whgewWqkQI8LH.skHBYML1.n8hc6','Migo','Steve','0612131415','1 rue Louis de Jabrun, 33000 Bordeaux',1);
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -66,3 +174,4 @@ INSERT INTO `theme` VALUES (5,'Classique'),(6,'Noël'),(7,'Pâques'),(8,'Événe
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2026-09-23 21:01:25
